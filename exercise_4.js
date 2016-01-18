6678b73a54b9a6e14131e668f0100c5472c0f21d
Student <student@RU-105.local>
2016-01-18 11:09:57 -0700

    began exercise 4

---
+++ exercise_4.js
@@ -0,0 +1,11 @@
+// on submit:
+	// gen new table row in respective table
+	// gen/add td for Name, phoneNumber, street
+		// document.getElementById("victims/volunteers").appendChild(td(content))
+
+function td(content) {
+	var element = document.createElement('td');
+	var text = document.createTextNode(text);
+	element.appendChild(text);
+	return element;
+}

886c5a682128dc86fd4c48399a04c0b6f9cffe97
Tom Lovett <tomlovett6@gmail.com>
2016-01-20 12:22:15 -0700

    zero real progress

--- ex4-1.js
+++ ex4-1.js
@@ -1,9 +1,29 @@
-angular.module('bananaTree', []); // separate module init from controller init
+// angular.module('bananaTree', []); // separate module init from controller init
 
-angular.module('bananaTree')
-	.controller('bananaTroller', ['$scope', function($scope) {
+// angular.module('bananaTree')
+// 	.controller('bananaTroller', ['$scope', function($scope) {
 
-		$scope.greeting = 'Sup, dude?';
+// 		$scope.greeting = 'Sup, dude?';
 
-	}
-]);
+// 	}
+// ]);
+
+angular.module('intro', []);  // init module
+
+angular.module('intro')
+	.controller('cntrls', ['$scope', function($scope) {
+
+		$scope.clickr = function() {
+			console.log('It worked!');
+			$scope.greeting = "Surprise mothertrucker!"
+		}
+
+		$scope.bigMistake = function() {
+			$scope.showMe = true;
+			$scope.title = 'You done goofed';
+			$scope.one = 'I\'ve called the cyber police';
+			$scope.two = 'I back-traced it!'
+			$scope.three = 'Consequences will never be the same!!';
+		}
+
+	}])
